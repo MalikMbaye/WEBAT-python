4ef9db28a4f862850eaa8fee4a51fa2069d6f972
Malik <mbayemalik@gmail.com>
2017-05-07 20:52:33 +0000

    added playing with inputs

---
+++ playing-with-inputs.py
@@ -0,0 +1,5 @@
+print "Hi, what's your name?"
+name = raw_input("Enter Name")
+print "Hi %r! What's your major?" % name
+major = raw_input("Enter Major")
+print "Got it! So %r is a %r major at Howard! Nice to meet you :)" % (name, major)
